e54b4a882561e94d4eabd7f357e3b1999f65de39
Frantsuzan_YS <niajulya@icloud.com>
2021-08-08 20:23:27 +0300

    initial and final

---
+++ HW_2.10/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  HW_2.10
+//
+//  Created by emmisar on 08.08.2021.
+//
+
+struct NumberFact: Decodable {
+    let text: String
+    let found: Bool
+}
+
--- HW_2.10/ViewController.swift
+++ HW_2.10/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var valueTextField: UITextField!
+    @IBOutlet var typeButtons: [UIButton]!
+    @IBOutlet var resultLabel: UILabel!
+    
+    var value = "random"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLabel.text = """
+            Hey! This application is for fun.
+            Press the button and find out an interesting fact about a number or date!
+            Good luck!
+            """
     }
+    
+    @IBAction func clickTypeButton(_ sender: UIButton) {
 
+        if let result = valueTextField.text {
+            if let _ = Int(result) {
+                value = result}
+            else {
+                if valueTextField.text == "" {
+                    value = "random"}
+                else {
+                    resultLabel.text = "Sorry! Please enter the correct number"
+                    return
+                }
+                }
+        }
+
+        let type = String(sender.title(for: .normal) ?? "trivia")
+        
+        guard let url = URL(string: "http://numbersapi.com/\(value)/\(type)?json") else {return}
+        
+        URLSession.shared.dataTask(with: url) {data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "Not results")
+                return
+            }
+            
+            do {
+                let numberFact = try JSONDecoder().decode(NumberFact.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.resultLabel.text = numberFact.text
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+         }.resume()
 
 }
-
+}
